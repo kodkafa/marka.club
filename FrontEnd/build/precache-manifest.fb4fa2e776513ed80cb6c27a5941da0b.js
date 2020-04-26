6d047fa44072bb0bd02ebc43aa1c4e0023c1ac76
goker <goker@cebeci.name>
2020-04-26 14:10:52 +0300

    frontend build/

---
+++ FrontEnd/build/precache-manifest.fb4fa2e776513ed80cb6c27a5941da0b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3dea53e3a84bfb8fc0891f465447818",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1135b55190ca7b7bbce",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "e8f43d580ee3104d49bd",
+    "url": "/static/css/main.acb63fad.chunk.css"
+  },
+  {
+    "revision": "b1135b55190ca7b7bbce",
+    "url": "/static/js/2.4ea9882a.chunk.js"
+  },
+  {
+    "revision": "9e95f6a96cc8c9a1d9afffecea5a5fc4",
+    "url": "/static/js/2.4ea9882a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8f43d580ee3104d49bd",
+    "url": "/static/js/main.b1b95669.chunk.js"
+  },
+  {
+    "revision": "0e6dd0d340c1517c0d54",
+    "url": "/static/js/runtime-main.9cf7e086.js"
+  },
+  {
+    "revision": "2d30c8a10b793e1aa5802ae735a525dc",
+    "url": "/static/media/avatar.2d30c8a1.svg"
+  },
+  {
+    "revision": "e2e6ab6e919133063032753b19179e74",
+    "url": "/static/media/intro-header-bg.e2e6ab6e.jpg"
+  }
+]);
